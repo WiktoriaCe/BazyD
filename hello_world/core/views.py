d96f7d2208399f277925d5dcd1460c59135c9113
Medical Researcher <researcher@medical-research.local>
2026-01-15 19:33:21 +0000

    Add beautiful landing page with API documentation and examples

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Django example",
+        "title": "Medical Research Database",
     }
-    return render(request, "index.html", context)
+    return render(request, "landing.html", context)
